578540edceb829d0ff716a0f25d69bc0e5f2ec70
rhit-perryca <perryca@rose-hulman.edu>
2023-05-11 16:23:13 -0400

    add email loading

---
+++ EmailProcessor.py
@@ -0,0 +1,31 @@
+
+from email import policy
+from email.parser import BytesParser
+import glob
+class Email:
+    def __init__(self, text,date):
+        self.text = text
+        self.date = date
+
+
+    def getText(self):
+        return self.text
+    def getDate(self):
+        return self.date
+def loadEmails():
+    path = './testEmails/'  # set this to "./" if in current directory
+    emails= []
+    eml_files = glob.glob(path + '*.eml')  # get all .eml files in a list
+    for eml_file in eml_files:
+        with open(eml_file, 'rb') as fp:  # select a specific email file from the list
+            name = fp.name  # Get file name
+            msg = BytesParser(policy=policy.default).parse(fp)
+        text = msg.get_body(preferencelist=('plain')).get_content()
+        fp.close()
+        text = text.split("\n")
+        newText = []
+        for line in text:
+            if(line != ""):
+                newText.append(line)
+        emails.append(Email('\n'.join(newText),msg.get('Date')))
+    return emails
